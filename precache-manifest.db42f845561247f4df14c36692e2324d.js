c67547ecb64108dd52702a4033ba0dcb290cd914
Rubin78 <rubinshaikh71@gmail.com>
2021-02-18 15:53:13 +0530

    Updates

---
+++ precache-manifest.db42f845561247f4df14c36692e2324d.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40605be1099cf6d2455b2947b47fc3d7",
+    "url": "/AminSiddique/index.html"
+  },
+  {
+    "revision": "292736bc8b392e5343e6",
+    "url": "/AminSiddique/static/css/2.459a22b9.chunk.css"
+  },
+  {
+    "revision": "22c3a1332dd93f79fcb8",
+    "url": "/AminSiddique/static/css/main.4e3fffca.chunk.css"
+  },
+  {
+    "revision": "292736bc8b392e5343e6",
+    "url": "/AminSiddique/static/js/2.2f65e47b.chunk.js"
+  },
+  {
+    "revision": "585c2ec942bb1f318866f751d5fe0574",
+    "url": "/AminSiddique/static/js/2.2f65e47b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22c3a1332dd93f79fcb8",
+    "url": "/AminSiddique/static/js/main.30a8772c.chunk.js"
+  },
+  {
+    "revision": "6db8ec68070ab06927e7",
+    "url": "/AminSiddique/static/js/runtime-main.865b2d07.js"
+  },
+  {
+    "revision": "1635b5d09d8729b6b1f80f2e9e7f3c4e",
+    "url": "/AminSiddique/static/media/Modelling1.1635b5d0.png"
+  },
+  {
+    "revision": "6326b1b8789bd06ce15a39d3c9f7aa95",
+    "url": "/AminSiddique/static/media/amin4.6326b1b8.png"
+  },
+  {
+    "revision": "6eb5ec380eda22c4ddb9cfb91ac17f4a",
+    "url": "/AminSiddique/static/media/five.6eb5ec38.png"
+  },
+  {
+    "revision": "522e1210ff6554d4f8d0f3adf324916c",
+    "url": "/AminSiddique/static/media/four.522e1210.png"
+  },
+  {
+    "revision": "fc5c08e2f211ed92c2b8183fec903544",
+    "url": "/AminSiddique/static/media/freepic.fc5c08e2.jpg"
+  },
+  {
+    "revision": "8354c47c324cddf656b7e584255f21b4",
+    "url": "/AminSiddique/static/media/nine.8354c47c.png"
+  },
+  {
+    "revision": "04e39e993b1a96d99f2d68a0a5822417",
+    "url": "/AminSiddique/static/media/port3.04e39e99.jpg"
+  },
+  {
+    "revision": "b8f14452ff8202102c00df8c1319b8e7",
+    "url": "/AminSiddique/static/media/seven.b8f14452.png"
+  },
+  {
+    "revision": "71b134b4f3aca648e545dbdd777b6870",
+    "url": "/AminSiddique/static/media/six.71b134b4.png"
+  },
+  {
+    "revision": "b0d7c5ab278538d875f0e310211d2e83",
+    "url": "/AminSiddique/static/media/ten.b0d7c5ab.png"
+  },
+  {
+    "revision": "b313f9ca957b592e3ff9d5be6b7436a1",
+    "url": "/AminSiddique/static/media/three.b313f9ca.png"
+  },
+  {
+    "revision": "e95d05630894e5403e569fdaa0a2b61a",
+    "url": "/AminSiddique/static/media/tosee2.e95d0563.webp"
+  }
+]);
